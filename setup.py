bdd1cdaa17fd86d45c211f1be81ce6689e8f45b8
Dmytro Lyfenko <d.lyfenko@gmail.com>
2023-01-30 12:32:01 +0200

    u

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='Helper_ua',
+    name='Helper_assistant',
     version='0.0.1',
     description='Personal Assistant "Helper" with a command line interface',
     url='https://github.com/MartynyukAndriy/Helper',
@@ -10,5 +10,5 @@ setup(
     license='MIT',
     include_package_data=True,
     packages=find_namespace_packages(),
-    entry_points={'console_scripts': ['helper = helper.helper:main']}
+    entry_points={'console_scripts': ['helper = Helper.helper:main']}
 )
